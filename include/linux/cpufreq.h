b64a4fb0d0309d174bf3b181cd923bcd8a1c2716
Erik Birgiser <ariesrik96@gmail.com>
2013-05-04 21:45:54 +0800

    cpufreq: lulzactiveW - combination of lulzactive and performance
    This governor is based on the Lulzactive governor by Tegrak.
    It replaces pump_up_step with max_up_step, now, when the CPU is solicited it will always scale up directly to the maximum frequency defined by the user. This tunning will result in more lagfree experience because the CPU will execute every task at its maximum potential.
    The battery drain is, of course, slightly higher than with the default Lulzactive settings, but we can notice a really good performance improvement.
    
    Signed off by: Erik Birgiser <ariesrik96@gmail.com>
    Signed-off-by: Keff <funnykeff91@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -388,6 +388,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVE)
 extern struct cpufreq_governor cpufreq_gov_lulzactive;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lulzactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVEW)
+extern struct cpufreq_governor cpufreq_gov_lulzactivew;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lulzactivew)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
 extern struct cpufreq_governor cpufreq_gov_hyper;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_hyper)
